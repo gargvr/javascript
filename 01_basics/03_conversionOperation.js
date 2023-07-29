387530cbd22a0473af33b6c4d0e9fd2b6c163cd6
Himanshu Garg <62766062+gargvr@users.noreply.github.com>
2023-07-29 11:16:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Himanshu"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+
+// "33" => 33
+// "33abc" => NaN (type number)
+// true =>1; false=> 0
+// null => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true
+//0 => false
+// "" => false
+//"someString" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
