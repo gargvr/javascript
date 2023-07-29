5a3e8d56513ae6e6c523dfc63cd7b37307dc9cc5
Himanshu Garg <62766062+gargvr@users.noreply.github.com>
2023-07-29 11:02:25 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello!") // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // Code readability should be high
+
+console.log('Himanshu')
+
+let name = "Himanshu"
+let age = 20
+let isLoggedIn = true
+let state;
+
+// number -> 2 to power of 53
+// bigInt (trading, stocks, facebook)
+// string -> ""
+// boolean -> true/false
+// null -> standalone value 
+// undefined -> 
+// symbol (reactJS) ->find unique
+
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
