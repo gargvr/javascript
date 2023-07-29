b8030bd7a6977b7faba1bfac20dda84c58aca1fe
Himanshu Garg <62766062+gargvr@users.noreply.github.com>
2023-07-29 11:47:32 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log( 2 > 1)
+// console.log( 2 >= 1)
+// console.log( 2 < 1)
+// console.log( 2 == 1)
+// console.log( 2 != 1)
+
+
+console.log("2" > 1)
+
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) //true // value conversion problem
+// Avoid above comparisons
+
+// === (check datatype also)
+
