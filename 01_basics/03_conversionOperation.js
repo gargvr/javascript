073288772e9e8d34bf80b24d5d6be7ea14dc46d1
Himanshu Garg <62766062+gargvr@users.noreply.github.com>
2023-07-29 11:36:08 +0000

    Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,44 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
+
+// ************************************* Operations ***********************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Himanshu"
+let str3 = str1 + str2
+
+console.log(str3) // hello Himanshu
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+
+console.log(+true) // 1
+console.log(+"") // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 // bad parctise
+
+let x = 3
+const y = x++
+
+console.log(`x:${x}, y:${y}`) // x:4, y:3
+
+let a = 3
+const b = ++a
+
+console.log(`a:${a}, b:${b}`) // a:4, b:4
